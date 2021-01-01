10c9b9a421a94177bb7b68b06513b3c034ae63c6
Jeongmin <tdcian71@gmail.com>
2021-01-01 19:47:16 +0900

    메모 모델 추가

---
+++ RxMemoWithMVVM/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxMemoWithMVVM
+//
+//  Created by APPLE on 2021/01/01.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String // 메모를 구분할 때 사용되는 속성
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
